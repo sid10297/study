6c1b3b1b212cf1a04bba0ce591ea405760e9921d
Siddharth Bhaiji <siddharth10297@gmail.com>
2025-01-19 17:46:54 +0530

    two sum

---
+++ leetcode/neetcode-roadmap/arrays-and-hashing/two-sum.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (nums, target) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+    if (map.hasOwnProperty(diff)) {
+      return [map[diff], i];
+    } else {
+      map[nums[i]] = i;
+    }
+  }
+};
