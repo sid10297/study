35a93943a07f8eb297f25a64991389cf06f7e361
Siddharth Bhaiji <siddharth10297@gmail.com>
2025-02-02 17:48:49 +0530

    valid palindrome with two pointer approach + use regex to improve per

--- leetcode/neetcode-roadmap/two-pointers/valid-palindrome.js
+++ leetcode/neetcode-roadmap/two-pointers/valid-palindrome.js
@@ -1,36 +1,57 @@
 const alphanumericCharacters = "abcdefghijklmnopqrstuvwxyz1234567890";
 
-/**
- * @param {string} s
- * @return {string}
- */
-function preparePalindromeString(s) {
-  const convertStrToLowercase = [...s.toLocaleLowerCase()];
-  let str = "";
-  for (const elem of convertStrToLowercase) {
-    if (alphanumericCharacters.includes(elem)) {
-      str += elem;
-    }
-  }
-  return str;
-}
+// /**
+//  * @param {string} s
+//  * @return {string}
+//  */
+// function preparePalindromeString(s) {
+//   const convertStrToLowercase = [...s.toLocaleLowerCase()];
+//   let str = "";
+//   for (const elem of convertStrToLowercase) {
+//     if (alphanumericCharacters.includes(elem)) {
+//       str += elem;
+//     }
+//   }
+//   return str;
+// }
 
-/**
- * @param {string} s
- * @return {string}
- */
-function reverse(s) {
-  return s.split("").reverse().join("");
-}
+// /**
+//  * @param {string} s
+//  * @return {string}
+//  */
+// function reverse(s) {
+//   return s.split("").reverse().join("");
+// }
+
+// /**
+//  * @param {string} s
+//  * @return {boolean}
+//  */
+// function isPalindrome(s) {
+//   const sanitizedString = preparePalindromeString(s);
+//   let reversedString = reverse(sanitizedString);
+//   return sanitizedString === reversedString ? true : false;
+// }
 
 /**
  * @param {string} s
  * @return {boolean}
  */
 function isPalindrome(s) {
-  const sanitizedString = preparePalindromeString(s);
-  let reversedString = reverse(sanitizedString);
-  return sanitizedString === reversedString ? true : false;
+  const input = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  let l = 0;
+  let r = input.length - 1;
+
+  while (l <= r) {
+    if (input[l] !== input[r]) {
+      return false;
+    } else {
+      l++;
+      r--;
+    }
+  }
+  return true;
 }
 
 console.log(isPalindrome("A man, a plan, a canal: Panama"));
